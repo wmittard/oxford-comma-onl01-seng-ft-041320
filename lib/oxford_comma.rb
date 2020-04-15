2d685590564b27a99ccf0ddf770f119395659653
William Mittard <billymittard@ymail.com>
2020-04-15 15:01:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+   return array.first if array.size == 1
 
+ 
+    if array.size == 2
+     array.insert(1, "and").join(" ")
+   elsif array.size > 2
+     array[-1].insert(0, "and ")
+     array.join(", ")
+   end
 end
